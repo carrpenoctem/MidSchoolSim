4df3aa248f19269d05ce692be0d06cff725c8266
carrpenoctem <dn.mski@gmail.com>
2019-05-02 19:50:07 +0200

    Animated Menu
    just some fun wth panels

---
+++ MiddleSchoolSimulator/Assets/Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    private static GameObject _activePanel;
+
+    public void ActivatePanel(GameObject _newPanel)
+    {
+        if(_activePanel == null)
+        {
+            _activePanel = _newPanel;
+            _activePanel.SetActive(true);
+            _activePanel.GetComponent<Animator>().Play("MainMenu_SidePane");
+        }
+        else if(_activePanel == _newPanel)
+        {
+            _activePanel.GetComponent<Animator>().Play("MainMenu_SidePaneClose");
+            _activePanel = null;
+        }
+        else
+        {
+            _activePanel.GetComponent<Animator>().Play("MainMenu_SidePaneClose");
+            _activePanel = _newPanel;
+            _activePanel.SetActive(true);
+            _activePanel.GetComponent<Animator>().Play("MainMenu_SidePane");
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
